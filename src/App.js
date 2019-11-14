3831718064dd94f290ec40f58893755386563bcc
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-11-14 08:16:13 -0600

    styled

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { useState } from 'react';
 import Form from './components/Form';
 import Members from './components/Members';
 import './App.css';
+import styled from 'styled-components';
+
+const MainTitle = styled.h1 `
+    color: lightblue;
+    text-shadow:
+		-1px -1px 0 #000,
+		1px -1px 0 #000,
+		-1px 1px 0 #000,
+		1px 1px 0 #000;
+`
+
+
 
 function App() {
   const [members, setMembers] = useState([
     {
       id: 1,
-      name: 'Johnny Hacker',
+      name: 'Ginny Hacker',
       email: 'yourmime@gotcha.com',
       role: 'Backend Whiz'
       
@@ -19,10 +31,16 @@ function App() {
       name: 'Phil Grinned',
       email: 'letstalk@hugz.com',
       role: 'UI Legend'  
-    }
+    },
+    {
+      id: 2,
+      name: 'Alison Noice',
+      email: 'dabestestever@me.com',
+      role: 'Front-End Engineer'  
+    } 
   ]);
 
-
+// add new member function
   const addNewMember = member => {
     const newMember = {
       id: Date.now(),
@@ -35,7 +53,7 @@ function App() {
 
   return (
     <div className="App">
-        <h1>Dev Superstars</h1>
+        <MainTitle>Dev Superstars</MainTitle>
         <Form addNewMember={addNewMember}/>
         <Members members={members} />
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,39 @@
 import React, { useState } from 'react';
+import styled from 'styled-components'
+
+const FormStyle = styled.div `
+    color: midnightblue
+    font-size: 1.6rem;
+
+`
+//styled role input
+const Role = styled.div `
+    margin-left: 5px;
+    
+`
+//styled Name input
+const Name = styled.span `
+    margin-right: 5px;
+`
+const Button = styled.button`
+    margin-left: 6.7%;
+    width: 12%;
+    background-color: dodgerblue;
+    font-size: .85rem;
+    color: white;
+    border-radius: 3px
+    
+`
 
 
+//Form to sign-up as a member
 const Form = (props) => {
     const [member, setMember] = useState({
     name: '', 
     email: '', 
     role: ''
 });
+//create new Member
     const handleNewMembers = event => {
         setMember({...member, [event.target.name] : event.target.value});
     }
@@ -17,29 +44,31 @@ const Form = (props) => {
         setMember({name: '', email: '', role: ''})
     }
     return (
-        <div>
+        <FormStyle>
           <form onSubmit={submitForm}>
-              <label>
-                  Your Name:
+              <Name>
+                   Name:
                   <input id='name' name='name' type='text' onChange={handleNewMembers} value={member.name}/>
-              </label>
+              </Name>
               <br/>
               <label>
-                  Your Email:
+                   Email:
                   <input id='email' name='email' type='text' onChange={handleNewMembers} value={member.email}/>
               </label>
               <br/>
-              <label>
-                  Your Role:
-                  <input id='role' name='role' type='text' onChange={handleNewMembers} value={member.role}/>
-              </label>
+              
+              <Role>
+                   Role :                   
+                  <input id='role' name='role' type='text' onChange={handleNewMembers} value={member.role}/>              
+              </Role>
+              
               <br/>
               
             
       
-              <button type='submit'>Make A Superstar</button>
+              <Button type='submit'>Be A Superstar</Button>
           </form>
-        </div>
+        </FormStyle>
 
     )
 }
